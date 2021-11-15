101d4fec5c31f8ecfbdb8d8e773b45d4265b4eef
SDK <admin@sundo.kim>
2021-11-15 23:29:48 +0900

    todo

--- src/nomadcoders/vanillajs-challenge/day01.js
+++ src/nomadcoders/vanillajs-challenge/day01.js
@@ -1,1 +1,36 @@
 // vanilla-js-challenge 1-day
+
+
+
+
+let toDoss = [];
+
+const toDoFrom = document.getElementById('test');
+
+// toDoFrom.addEventlis
+
+JSON.stringify(toDoFrom);
+
+JSON.stringify([1, 2, 3 , 4]);
+
+JSON.parse("[1, 2, 3, 4, 5]");
+
+const TODOS_KEY = "todos";
+
+
+const hello = [1, 2,3, 4, 5];
+
+hello.forEach((i) =>console.log(i));
+
+const test = localStorage.getItem("test");
+
+function paintToDo(item) {
+
+}
+
+if(test) {
+    const parseTodos = JSON.parse(test);
+    toDos = parseTodos;
+    parseTodos.forEach(paintToDo);
+}
+
